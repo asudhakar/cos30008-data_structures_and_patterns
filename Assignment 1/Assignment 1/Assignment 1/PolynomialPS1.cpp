23bc49d4e7853c7d3a50e0a3a1880eed8a513de5
ShiroKatake <tgkhang262@gmail.com>
2021-03-25 11:06:40 +1100

    UPDATE #A1.1.2: Implemented () operator

--- Assignment 1/Assignment 1/Assignment 1/PolynomialPS1.cpp
+++ Assignment 1/Assignment 1/Assignment 1/PolynomialPS1.cpp
@@ -1,22 +1,31 @@
 // COS30008, Tutorial 3, 2021
 
 #include "Polynomial.h"
-#include "Polynomial.cpp"
+#include <math.h>
 
 using namespace std;
 
 double Polynomial::operator()(double aX) const {
+	double result = 0;
+	for (int i = fDegree; i >= 0; i--)
+	{
+		result += fCoeffs[i] * pow(aX, i);
+	}
 
+	return result;
 }
 
 Polynomial Polynomial::getDifferential() const {
-
+	Polynomial a;
+	return a;
 }
 
 Polynomial Polynomial::getIndefiniteIntegral() const {
-
+	Polynomial a;
+	return a;
 }
 
 double Polynomial::getDefiniteIntegral(double aXLow, double aXHigh) const {
-
+	double b = 0;
+	return b;
 }
