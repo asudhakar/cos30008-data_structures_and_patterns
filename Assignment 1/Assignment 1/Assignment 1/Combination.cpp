b2e0f49ac9a5035ac45eb81eabe61d0d75135f61
ShiroKatake <tgkhang262@gmail.com>
2021-03-26 17:40:52 +1100

    UPDATE #A1.2.1: Combination implementation

---
+++ Assignment 1/Assignment 1/Assignment 1/Combination.cpp
@@ -0,0 +1,30 @@
+#include "Combination.h"
+#include <iostream>
+
+using namespace std;
+
+Combination::Combination(unsigned int aN, unsigned int aK) {
+	fN = aN;
+	fK = aK;
+}
+
+unsigned int Combination::getN() const {
+	return fN;
+}
+
+unsigned int Combination::getK() const {
+	return fK;
+}
+
+unsigned long long Combination::operator()() const {
+	if (fK > fN) return 0;
+	if (fK == fN) return 1;
+
+	unsigned long long lResult = fN;
+	for (int k = 1; k <= fK; k++)
+	{
+		lResult *= ((unsigned long long)fN - k + 1);
+		lResult /= k;
+	}
+	return lResult;
+}
