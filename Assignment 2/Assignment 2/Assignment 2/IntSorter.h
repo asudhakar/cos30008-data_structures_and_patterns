be821d2fc625b1f1a862489928f14f6abb36a30d
ShiroKatake <tgkhang262@gmail.com>
2021-04-14 10:38:21 +1000

    ADD #A2.0.0: Assignment 2 initial commit

---
+++ Assignment 2/Assignment 2/Assignment 2/IntSorter.h
@@ -0,0 +1,14 @@
+
+#pragma once
+
+// forward declaration for container
+class IntVector;
+
+class IntSorter
+{
+public:
+    
+    virtual ~IntSorter() {}
+    
+    virtual void operator()( IntVector& aContainer ) const {}
+};
