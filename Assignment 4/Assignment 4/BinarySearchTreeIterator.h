996f5e4eecc8ce437e77dd44d4c1b7dc1a3e033c
ShiroKatake <tgkhang262@gmail.com>
2021-05-22 17:12:44 +1000

    UPDATE #A4.3.0: Part 3 started

--- Assignment 4/Assignment 4/BinarySearchTreeIterator.h
+++ Assignment 4/Assignment 4/BinarySearchTreeIterator.h
@@ -4,6 +4,7 @@
 #pragma once
 
 #include <stack>
+#include <iostream>
 
 #include "BNode.h"
 
@@ -18,16 +19,64 @@ public:
     
     using Iterator = BinarySearchTreeIterator<T>;
     
-	BinarySearchTreeIterator( const BNode<T>* aBNodeTree );
+	BinarySearchTreeIterator(const BNode<T>* aBNodeTree) 
+		: fBNodeTree(aBNodeTree)
+	{
+		if (!fBNodeTree->empty())
+		{
+			fStack.push(fBNodeTree);
 
-	const T& operator*() const;
+			while (!fStack.top()->left->empty())
+				fStack.push(fStack.top()->left);
+		}
+		std::cout << fStack.top() << endl;
+	}
 
-	Iterator& operator++();
-	Iterator operator++(int);
+	const T& operator*() const {
+		return fBNodeTree->key;
+	}
 
-	bool operator==( const Iterator& aOtherIter ) const;
-	bool operator!=( const Iterator& aOtherIter ) const;
+	Iterator& operator++() {
+		/*
+		If the current node has a non-null right child,
+			Take a step down to the right
+			Then run down to the left as far as possible
+
+		If the current node has a null right child,
+			move up the tree until we have moved over a left child link*/
+
+		if (!fBNodeTree->empty() || !fStack.empty())
+		{
+			while (fBNodeTree != NULL)
+			{
+				fStack.push(fBNodeTree);
+				fBNodeTree = fBNodeTree->left;
+			}
+
+			fBNodeTree = fStack.top();
+			fStack.pop();
+
+			std::cout << fBNodeTree->key << " ";
+			fBNodeTree = fBNodeTree->right;
+		}
+		return *this;
+	}
+	Iterator operator++(int) {
+		Iterator iter = *this;
+		return iter;
+	}
+
+	bool operator==(const Iterator& aOtherIter) const {
+		return fBNodeTree->key == aOtherIter.fBNodeTree->key;
+	}
+	bool operator!=(const Iterator& aOtherIter) const {
+		return fBNodeTree->key != aOtherIter.fBNodeTree->key;
+	}
     
-	Iterator begin() const;
-	Iterator end() const;
+	Iterator begin() const {
+		return Iterator(fBNodeTree->findMin());
+	}
+	Iterator end() const {
+		return Iterator(&BNode<T>::NIL);
+	}
 };
